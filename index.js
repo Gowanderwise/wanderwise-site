e0bff6cf0676c9b00ce660dc042a4e6b2a39b05b
Gowanderwise <gowanderwisenow@gmail.com>
2025-06-04 21:45:32 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to WanderWise</h1>
+      <p>Plan Smarter. Wander Wiser.</p>
+    </main>
+  )
+}
