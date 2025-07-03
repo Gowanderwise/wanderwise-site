6f830e475418c13b3de9b2b6af5a04e24c9bff4a
Gowanderwise <gowanderwisenow@gmail.com>
2025-07-02 20:56:40 -0700

    Update _app.js
    Added Export Default

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,6 @@
-import '../styles/globals.css'; // or wherever your tailwind CSS is located
+// pages/_app.js
+import '../styles/globals.css'; // Adjust if your CSS file is named differently
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
